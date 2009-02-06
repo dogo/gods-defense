fd1fa1f3fe3367e44b0c8f0f19d66f2e3f2eefff
diautilio <diautilio@85b660d0-e80c-11dd-9b7c-bbba4c9295f9>
2009-02-06 16:00:28 +0000

    -Updated "PRESS_ANYKEY" String, now is flashing.

--- PSP Project/Src/TitleScreen.cpp
+++ PSP Project/Src/TitleScreen.cpp
@@ -15,8 +15,13 @@ void TitleScreen::draw()
 {
 	oslIntraFontSetStyle(gFont, 3.0f,RGBA(255,255,255,255), RGBA(0,0,0,0),INTRAFONT_ALIGN_CENTER); //Tells the PSP what size and shape the text is
 	oslDrawString(240,120,Resource::GAME_TITLE);
-	oslIntraFontSetStyle(gFont, 1.0f,RGBA(255,255,255,255), RGBA(0,0,0,0),INTRAFONT_ALIGN_CENTER);
+	
+	//Flashing
+	float t = ((float)(clock() % CLOCKS_PER_SEC)) / ((float)CLOCKS_PER_SEC);
+	int val = (t < 0.5f) ? t*511 : (1.0f-t)*511;
+	oslIntraFontSetStyle(gFont, 1.0f,RGBA(255,255,255,255),(0xFF<<24)+(val<<16)+(val<<8)+(val),INTRAFONT_ALIGN_CENTER);
 	oslDrawString(240,220,Resource::PRESS_ANYKEY);
+	oslIntraFontSetStyle(gFont, 1.0f,RGBA(255,255,255,255), RGBA(0,0,0,0),INTRAFONT_ALIGN_CENTER);
 }
 
 void TitleScreen::update()
