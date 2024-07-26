07efcaba8ba5b558b3112a9eb2f4e1077aaaaef4
Diogo Autilio <diautilio@gmail.com>
2024-07-26 09:53:07 -0300

    MenuItem refactor

---
+++ PSP Project/Include/util/MenuItem.h
@@ -0,0 +1,15 @@
+//**************************************************************************
+//		PSP Project: 'Gods Defense' - Fps.h
+//		@author Diogo Autilio <A.K.A Dogo - diautilio@gmail.com>
+//**************************************************************************
+
+#ifndef MENUITEM_H
+#define MENUITEM_H
+
+struct MenuItem
+{
+    const char* text;
+    int yPosition;
+};
+
+#endif // MENUITEM_H
--- PSP Project/Src/MainMenuScreen.cpp
+++ PSP Project/Src/MainMenuScreen.cpp
@@ -4,34 +4,31 @@
 //**************************************************************************
 
 #include "../Include/MainMenuScreen.h"
+#include "../Include/util/MenuItem.h"
 #include "../Include/ScreenManager.h"
 
 bool gIsMultiplayer = false;
 
 MainMenuScreen::MainMenuScreen()
 {
-	gIsMultiplayer = false;
-	menubg = oslLoadImageFilePNG(Resource::IMG_MAIN_MENU_BG, OSL_IN_RAM | OSL_SWIZZLED, OSL_PF_8888);
+    gIsMultiplayer = false;
+    menubg = oslLoadImageFilePNG(Resource::IMG_MAIN_MENU_BG, OSL_IN_RAM | OSL_SWIZZLED, OSL_PF_8888);
 
-	if (!menubg)
-		oslFatalError("At least one file is missing. Please copy all the file in the game directory.");
+    if (!menubg) {
+        oslFatalError("At least one file is missing. Please copy all the files in the game directory.");
+    }
 }
 
 MainMenuScreen::~MainMenuScreen()
 {
-	if(menubg != NULL)
-		oslDeleteImage(menubg);
+    if (menubg != NULL) {
+        oslDeleteImage(menubg);
+    }
 }
 
 void MainMenuScreen::draw()
 {
     oslDrawImageXY(menubg, (480/2) - (menubg->stretchX/2), (272/2) - (menubg->stretchY/2));
-    
-    struct MenuItem
-    {
-        const char* text;
-        int yPosition;
-    };
     
     MenuItem menuItems[] = {
         {Resource::STR_START_GAME, 100},
@@ -41,7 +38,7 @@ void MainMenuScreen::draw()
         {Resource::STR_ABOUT_CAPTION, 180},
         {Resource::STR_EXIT_CAPTION, 200}
     };
-    
+
     for (int i = 0; i < sizeof(menuItems)/sizeof(menuItems[0]); ++i)
     {
         if (gMenu == i)
@@ -56,61 +53,50 @@ void MainMenuScreen::draw()
     }
 }
 
-void MainMenuScreen::update(u32 /*timePassed*/) //Parametro Formal, n�o d� warning
+void MainMenuScreen::update(u32 /*timePassed*/) // Prevents compiler warning for unused parameter
 {
-	//menu keys
-	if(osl_keys->pressed.up){
-		gMenu--;
-		if (gMenu < 0)
-		{
-			gMenu = 5;
-		}
-	}
-	if(osl_keys->pressed.down){
-		gMenu++;
-		gMenu%=6;
-	}
-	if(osl_keys->pressed.cross){
-		if(gMenu == 0)
-		{
-			oslFlushKey();
-			mNextScreen = ScreenManager::SCREEN_GAME_OPTIONS; // start game options!
-		}
-		if(gMenu == 1)
-		{
-			oslFlushKey();
+    // Handle menu navigation
+    if (osl_keys->pressed.up) {
+        gMenu--;
+        if (gMenu < 0) {
+            gMenu = 5;
+        }
+    }
+    if (osl_keys->pressed.down) {
+        gMenu++;
+        gMenu %= 6;
+    }
+    if (osl_keys->pressed.cross) {
+        oslFlushKey();
+        switch (gMenu) {
+            case 0:
+                mNextScreen = ScreenManager::SCREEN_GAME_OPTIONS; // Start game options!
+                break;
+            case 1:
 #ifndef JPCSP_EMULATOR
-			if(oslIsWlanPowerOn())
-			{
-				gIsMultiplayer = true;
-				mNextScreen = ScreenManager::SCREEN_MULTIPLAYER;
-			}
-			else
-				mNextScreen = ScreenManager::SCREEN_WARNING;
+                if (oslIsWlanPowerOn()) {
+                    gIsMultiplayer = true;
+                    mNextScreen = ScreenManager::SCREEN_MULTIPLAYER;
+                } else {
+                    mNextScreen = ScreenManager::SCREEN_WARNING;
+                }
 #else
-			gIsMultiplayer = true;
-			mNextScreen = ScreenManager::SCREEN_MULTIPLAYER;
+                gIsMultiplayer = true;
+                mNextScreen = ScreenManager::SCREEN_MULTIPLAYER;
 #endif // JPCSP_EMULATOR
-		}
-		if(gMenu == 2)
-		{
-			oslFlushKey();
-			mNextScreen = ScreenManager::SCREEN_HELP;
-		}
-		if(gMenu == 3)
-		{
-			oslFlushKey();
-			mNextScreen = ScreenManager::SCREEN_TROPHIE;
-		}
-		if(gMenu == 4)
-		{
-			oslFlushKey();
-			mNextScreen = ScreenManager::SCREEN_ABOUT;
-		}
-		if(gMenu == 5)
-		{
-			oslFlushKey();
-			mNextScreen = ScreenManager::SCREEN_CONFIRM_EXIT;
-		}
-	}
+                break;
+            case 2:
+                mNextScreen = ScreenManager::SCREEN_HELP;
+                break;
+            case 3:
+                mNextScreen = ScreenManager::SCREEN_TROPHIE;
+                break;
+            case 4:
+                mNextScreen = ScreenManager::SCREEN_ABOUT;
+                break;
+            case 5:
+                mNextScreen = ScreenManager::SCREEN_CONFIRM_EXIT;
+                break;
+        }
+    }
 }
